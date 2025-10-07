5abf8e311cce191096a847a122558ba7ea5b0209
Mary-Samuela <samuelamary@gmail.com>
2025-10-07 15:49:12 +0300

    add style

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-document.getElementById("todo-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    const input = document.getElementById("todo-input");
-    const task = input.value.trim();
-    if (task) {
-        const li = document.createElement("li");
-        li.textContent = task; 
-        document.getElementById("todo-list").appendChild(li);
-        input.value = "";
-    }
-});
+
+const toDoInput = document.getElementById("todo-input");
+const toDoList = document.getElementById("todo-list");
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+})
