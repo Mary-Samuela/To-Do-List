44fb91649851bc54437d6b998c1289969de597c3
Mary-Samuela <samuelamary@gmail.com>
2025-10-08 16:49:26 +0300

    add user interface

--- script.js
+++ script.js
@@ -24,4 +24,51 @@ class ToDoList {
 }
 
 
-  
+//   User Interface Logic
+
+const todo = new ToDoList();
+const taskInput = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const taskList = document.getElementById("taskList");
+
+addBtn.addEventListener("click", () => {
+    const title = taskInput.value.trim();
+    if (title) {
+        todo.addtTask(title);
+        taskInput.value = "";
+        render();
+    }
+});
+
+
+function render() {
+    taskList.innerHTML = "";
+    todo.tasks.forEach((task, index) => {
+        const li = document.createElement("li");
+        li.textContent = task.title;
+        if (task.done) li.classList.add("done");
+
+        const actions = document.createElement("div");
+
+        const doneBtn = document.createElement("button");
+        doneBtn.textContent = "v"
+        doneBtn.onClick = () => {
+            todo.markDone(index);
+            render();
+        };
+
+
+        const removeBtn = document.createElement("button");
+        removeBtn.textContent = "#";
+        removeBtn.onclick = () => {
+            todo.removeTask(index);
+            render();
+        };
+
+        actions.append(doneBtn, removeBtn);
+        li.appendChild(actions);
+        taskList.appendChild(li);
+    });
+}
+
+render();
