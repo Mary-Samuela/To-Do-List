d70069033f32e0e78ecb01115d0a5d3c819d4a2d
Mary-Samuela <samuelamary@gmail.com>
2025-10-08 16:10:55 +0300

    redefine the work

--- script.js
+++
@@ -1,8 +0,0 @@
-
-const toDoInput = document.getElementById("todo-input");
-const toDoList = document.getElementById("todo-list");
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    
-})
